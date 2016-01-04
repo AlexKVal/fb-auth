cdc8d041e8d9a1427966515d11bd14c8b0996311
AlexKVal <alexkval@gmail.com>
2016-01-05 01:22:09 +0200

    [added] mainly boilerplate and auto-generated scaffolding
    also set firebase hosting and ember-cli version for 'emberfirebase'

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'fb-auth',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://chapalu.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
