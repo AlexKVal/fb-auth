1dc5a02c930bbe9ea9b900ec48ee7e726f5f79ea
AlexKVal <alexkval@gmail.com>
2016-01-07 18:13:13 +0200

    Define models and their relations

--- app/models/post.js
+++ app/models/post.js
@@ -1,5 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  title: DS.attr('string'),
+  body: DS.attr('string'),
+  titleURL: DS.attr('string'),
+  date: DS.attr('date'),
+
+  comments: DS.hasMany('comment'),
+  user: DS.belongsTo('user')
 });
--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+    uid: DS.attr('string'),
+    username: DS.attr('string'),
+    avatar: DS.attr('string'),
+
+    comments: DS.hasMany('comment'),
+    posts: DS.hasMany('post')
 });
