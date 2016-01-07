fbd1aaab2bb51d406622906aeb041fa2e690bae6
AlexKVal <alexkval@gmail.com>
2016-01-07 18:08:43 +0200

    Use the title of a post as a route (a slug)

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('new');
-  this.route('posts');
+  this.route('posts', { path: '/:titleURL' });
 });
 
 export default Router;
