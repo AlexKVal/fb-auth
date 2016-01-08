e66e291efde85efb97d3360c1f3dcc5f64cf6c76
AlexKVal <alexkval@gmail.com>
2016-01-08 21:59:48 +0200

    [added] 'add-comments' component

--- app/components/add-comments.js
+++ app/components/add-comments.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    submitComment(author, body) {
+      const post = this.get('post');
+      this.sendAction('store', author, body, post);
+      this.setProperties({ body: '' });
+    }
+  }
 });
