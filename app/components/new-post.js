b41436c7e0f51ba3acf0af55b3987478e242715e
AlexKVal <alexkval@gmail.com>
2016-01-10 19:52:12 +0200

    [added] 'new-post' component

--- app/components/new-post.js
+++ app/components/new-post.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: 'new',
+  actions: {
+    save(title, body) {
+      this.sendAction('save', title, body);
+      this.set('title', '');
+      this.set('body', '');
+    }
+  }
 });
