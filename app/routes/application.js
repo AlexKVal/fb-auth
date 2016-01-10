e398c35f88da6c4be6e702b421002501f4425a48
AlexKVal <alexkval@gmail.com>
2016-01-10 20:11:21 +0200

    [added] 'application' route

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  beforeModel() {
+    return this.get('session').fetch().catch(function() {});
+  },
+
+  model() {
+    return this.store.findAll('post');
+  },
+
+  actions: {
+    login() {
+      this.get('session')
+        .open('firebase', { provider: 'twitter' })
+        .then(function(data) {
+          console.log(data);
+        });
+    },
+
+    logout() {
+      this.get('session').close();
+    }
+  }
 });
